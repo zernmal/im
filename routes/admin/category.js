443344f0c3631bcfbc5a1b69b4f0fd73e58d1fc0
zernmal <zernmal@foxmail.com>
2013-12-20 00:47:31 +0800

    add category template update

--- routes/admin/category.js
+++ routes/admin/category.js
@@ -1,7 +1,9 @@
 module.exports.index = function(req, res){
 	
-	getLibFile("db");
-	//console.log(dbConnection);
 	res.render('admin/category/index', { title: '栏目列表' });	
 	
 };
+
+module.exports.add = function(req,res){
+	res.render("admin/category/add",{title:'添加栏目'});
+}
